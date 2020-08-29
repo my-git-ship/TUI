022f47c5e457fea6dbad80729253a36026457bf3
my-git-ship <mail7368841291@gmail.com>
2020-08-29 08:45:20 +0530

    update 5

--- tui.py
+++ tui.py
@@ -5,7 +5,10 @@ os.system("tput setaf 1")
 # To print the statement with 3 tab.
 print("\t\t\tWelcome to TUI")
 # To change the font colour to white.
-print("\t\t\t--------------")
+os.system("tput setaf 7")
+print("\t\t\t==============")
+
+os.system("tput setaf 1")
 
 print("""press 1 : TO see the date
 press 2 : To see the calander
@@ -14,4 +17,12 @@ press 4 : To see the list of files and directories
 press 5 : To see the details of files and directories
 """)
 
- 
+os.system("tput setaf 7")
+
+choice=input("Enter your choice : ")
+
+if int(choice) == 1:
+	os.system("date")
+else:
+	print("Option not supported , try for the next option")
+	
