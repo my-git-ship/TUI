932db147c0df38423719fd05fb6e4db3ec7e78fb
my-git-ship <mail7368841291@gmail.com>
2020-08-29 08:19:17 +0530

    update 3

--- tui.py
+++ tui.py
@@ -1,6 +1,9 @@
 # Program for TUI.
 import os
-# To change the fount colour to red.
+# To change the font colour to red.
 os.system("tput setaf 1")
 # To print the statement with 3 tab.
 print("\t\t\tWelcome to TUI")
+# To change the font colour to white.
+print("\t\t\t--------------")
+
